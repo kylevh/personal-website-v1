3ff00022c5e5c4d67f9863a23ee8a51e1760998b
Kyle Huynh <kyle_huynh@hotmail.com>
2022-05-31 12:26:00 -0700

    description updates

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import about from '../img/about.jpg';
-import {    Link } from 'react-router-dom';
+import React from "react";
+import about from "../img/about.jpg";
+import { Link } from "react-router-dom";
 
 function ImageSection() {
-    return (
-        <div className="ImageSection">
-            <div className="img-holder">
-                <img src={about} alt="" />
-            </div>
-            <div className="about-info">
-                <h4>I am a<span> Software Engineer</span></h4>
-                <p className="about-text">
-                    I'm currently an undergraduate in my third year studying Computer Science and Software Engineering
-                    at the <span>University of Washington Bothell</span>.
-                </p>
-                <p className="about-text-2">
-                    I work as a web assistant for the UW Department of Lab Medicine and manage the
-                    the Husky Game Dev Club as vice-president, which is dedicated to teaching students the process of game development and coding.
-                </p>
-                <p className="about-text-2">
-                    Outside of programming, I enjoy cooking, gaming, doing graphic design, and film photography.
+  return (
+    <div className="ImageSection">
+      <div className="img-holder">
+        <img src={about} alt="" />
+      </div>
+      <div className="about-info">
+        <h4>
+          I am a<span> Software Engineer</span>
+        </h4>
+        <p className="about-text">
+          I'm currently an undergraduate in my third year studying Computer
+          Science and Software Engineering at the{" "}
+          <span>University of Washington Bothell</span>.
+        </p>
+        <p className="about-text-2">
+          I work as a web assistant for the UW Department of Lab Medicine and
+          manage the the Husky Game Dev Club as vice-president, which is
+          dedicated to teaching students the process of game development and
+          coding.
+        </p>
+        <p className="about-text-2">
+          Outside of programming, I enjoy doing graphic design, kendo, film
+          photography, and cooking.
+        </p>
 
-                </p>
-
-
-                {/* <div className="about-details">
+        {/* <div className="about-details">
                     <div className="left-section">
                         <p>Full Name</p>
                         <p>Age</p>
@@ -40,12 +44,12 @@ function ImageSection() {
                         <p>: Computer Science and Software Engineering</p>
                     </div>
                 </div> */}
-                <Link to="/kvh_resume.pdf" target="_blank" download>
-                    <button className="btn">Download Resume</button>
-                </Link>
-            </div>
-        </div>
-    )
+        <Link to="/kvh_resume.pdf" target="_blank" download>
+          <button className="btn">Download Resume</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default ImageSection
+export default ImageSection;
--- src/Components/contactMe.js
+++ src/Components/contactMe.js
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function contactMe() {
-    return (
-        <div className="contactMe">
-            <header className="hero">
-                <h1 className="hero-text">
-                    Get in touch with me
-                </h1>
-                <p className="hero-sub-text" >
-                    I'm currently looking for internship oppurtunities in the field of Software Engineering for Summer 2022.
-                    Feel free to contact me with any questions!
-                </p><br/>
-            </header>
+  return (
+    <div className="contactMe">
+      <header className="hero">
+        <h1 className="hero-text">Get in touch with me</h1>
+        <p className="hero-sub-text">
+          I'm currently looking for internship oppurtunities in the field of
+          Software Engineering for Summer/Fall 2022. Feel free to contact me
+          with any questions!
+        </p>
+        <br />
+      </header>
 
-
-            <a href class="email-link" href="mailto:kylevh@outlook.com">
-                <button className="btn-contact">Contact me</button>
-            </a>
-        </div>
-    )
+      <a href class="email-link" href="mailto:kylevh@outlook.com">
+        <button className="btn-contact">Contact me</button>
+      </a>
+    </div>
+  );
 }
 
-export default contactMe
+export default contactMe;
